3758f8eecf8013ffb36941f30e5a1b5dba627829
BinouX <liebaertb@gmail.com>
2017-01-05 16:15:22 +0100

    modif access

--- class/comment.php
+++ class/comment.php
@@ -1,20 +1,44 @@
 <?php
+  include "./access.php";
+
+  $_access = new Access();
+  $_comment = new Comment($_access);
+
+  $_comment->addCommentTOBdd("Le java c est trop genial", 0 , 1);
+
   class Comment{
     private $_comment ="";
-
-
+    private $_access ="";
     /**
      * Get the value of Comment
      *
      * @return mixed
      */
+
+     public function __construct($object){
+       $this->setAccess($object);
+     }
+
+     /**
+      * Set the value of Access
+      *
+      * @param mixed _access
+      *
+      * @return self
+      */
+     public function setAccess($_access)
+     {
+         $this->_access = $_access;
+         return $this;
+     }
+
     public function getComment()
     {
         return $this->_comment;
     }
 
     /**
-     * Set the value of Comment 
+     * Set the value of Comment
      *
      * @param mixed _comment
      *
@@ -26,6 +50,19 @@
 
         return $this;
     }
+
+    public function addCommentToBdd($comment, $styleComment, $id){
+      $this->setComment($comment);
+      $classId = NULL; $noteId = NULL; $studentId= NULL; $listnoteId=1;
+
+      $table = "aston_commentaire";
+      $fields = "comment, aston_classe_id, aston_note_id, aston_student_id, aston_listnote_id";
+      $values = $this->getComment().",".$classId.",".$noteId.",".$studentId.",".$listnoteId;
+
+      $this->_access->new_cnx();
+      //$_access->insert($table,$fields,$values);
+    }
+
 
 }
  ?>
