cf6a1c212721ac0491334bcc6fd5c59749c7979b
sorren <alexis.verquin@gmail.com>
2017-01-05 14:04:34 +0100

    Initialisation du prjet

---
+++ includes/foot.php
@@ -0,0 +1,5 @@
+    <script src="../aston/assets/js/jquery.js"></script>
+    <script src="../aston/assets/js/script.js"></script>
+    </body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include 'includes/head.php';?>
+<?php include 'includes/header.php';?>
+<section>
+    adazda
+</section>
+<?php include 'includes/footer.php'; ?>
+<?php include 'includes/foot.php'; ?>
