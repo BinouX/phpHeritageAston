f023a0f7631477c0c0bc4753094e838b7de7a40e
sorren <alexis.verquin@gmail.com>
2017-01-05 15:33:26 +0100

    front

---
+++ gestion.php
@@ -0,0 +1,19 @@
+<?php include 'includes/head.php';?>
+<?php include 'includes/header.php';?>
+<section id="home">
+    
+        <center>
+     <div id="panel">
+            <H1>Panel Administration</H1>
+            <H2>Veuillez vous connecter pour accéder aux nots</H2>
+        </div>
+    
+        </center>
+
+       
+    
+    
+   
+</section>
+<?php include 'includes/footer.php'; ?>
+<?php include 'includes/foot.php'; ?>
