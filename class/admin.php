1493f90ef7eaf82b35f5e4d1900060a57d3003fc
forget59 <fabrice.forget.59@gmail.com>
2017-01-06 11:28:24 +0100

    correction admin

--- class/admin.php
+++ class/admin.php
@@ -2,14 +2,14 @@
   include 'people.php';
 
   class Admin extends People{
-      addStudent($student){
+      public function addStudent($student){
       	$access = new Access();
 		$access->new_cnx();
 		$fields = "`id_personne`, `firstname_student`, `lastname_student`, `password_student`,`id_classe`";
         $values = "NULL, '".$student->getFirstname()."', '".$student->getLastname()."', '".$student->getPassword()."', '".$student->getIdClasse()."'";
         $access->insert("aston_student",$fields,$values);
       }
-      addTeacher($teacher){
+      public function addTeacher($teacher){
       	$access = new Access();
 		$access->new_cnx();
 		$fields = "`id_teacher`, `firstname_teacher`, `lastname_teacher`, `password_teacher`";
