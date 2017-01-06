b0d5e521c5cdeb20a3da4cf9f11f14ed502d8cff
antares667367 <logrus66@gmail.com>
2017-01-06 11:46:10 +0100

    classe classeManager finie

--- class/classeManager.php
+++ class/classeManager.php
@@ -1,10 +1,11 @@
 <?php
 
 include 'access.php';
+include 'studentManager.php';
 
 /**
  * Created by PhpStorm.
- * User: michel
+ * User: thomas Aldeguer
  * Date: 06/01/2017
  * Time: 09:23
  */
@@ -28,13 +29,14 @@ class classeManager
     }
 
     public function insert($classe,$com=" "){
+        $m = new studentManager();
         $access = new Access();
         $access->new_cnx();
         $table = 'classe';
         $nom=$classe->getName();
-        $deleg1=$classe->getDelegate1->getName();
-        $deleg2=$classe->getDelegate2->getName();
-        $fields="";
+        $deleg1=$m->select_one('id_student',"firstname LIKE %".$classe->getDelegate1->getFirstname."%");
+        $deleg2=$m->select_one('id_student',"firstname LIKE %".$classe->getDelegate2->getFirstname."%");
+        $fields="name_classe,delegate1_classe,delegate2_classe,com_classe";
         $values="".$nom.",".$deleg1.",".$deleg2.",".$com."";
         $access->insert($table,$fields,$values);
     }
